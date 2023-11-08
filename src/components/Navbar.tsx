4b3e5f6baa8cd273ea287162f8e6c8601c91058d
Rodrigo Gomes <mariadefatima071965@gmail.com>
2023-11-08 14:46:44 -0300

    feat: create and add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import { BookOpenCheck } from "lucide-react";
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <div className="navbar bg-neutral-100">
+      <div className="container">
+        <div className="flex-1">
+          <Link href="/">
+            <BookOpenCheck color="blue" />
+          </Link>
+        </div>
+        <div className="flex-none">
+          <Link href="/create" className="btn btn-ghost">
+            Create Post
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
